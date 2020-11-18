b0b65769a500f8b960ab284ab93d37557979f143
Emal Fiza <emal_fiza@hotmail.com>
2020-11-18 00:38:59 +0000

    Sidebar and feeds sections completed

---
+++ twitter-clone-qazi/src/TweetBox.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import "./TweetBox.css"
+import { Avatar, Button } from "@material-ui/core";
+
+function TweetBox() {
+    return (
+        <div className="tweetBox">
+            <form>
+                <div className="tweetBox__input">
+                    <Avatar alt="Cindy Baker" src="/static/images/avatar/3.jpg" />
+                    <input placeholder="What's happening?" type="text"/>
+                </div>
+                <input className="tweetBox__imageInput" placeholder="Enter image URL" type="text"/>
+                <Button className="tweetBox__tweetButton">Tweet</Button>
+            </form>
+        </div>
+    )
+}
+
+export default TweetBox
